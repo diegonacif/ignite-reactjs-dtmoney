7b4b07963e45964bc8421a33a539789b9d05d657
diegonacif <diegoreisnacif@gmail.com>
2021-09-24 06:51:01 -0300

    resolvido bug da adicao de nova transacao e concluido o fechamento do modal

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -8,18 +8,18 @@ interface Transaction {
   type: string;
   category: string;
   createdAt: string;
-}
+};
 
 type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
 
 interface TransactionsProviderProps {
   children: ReactNode;
-}
+};
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
-}
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
+};
 
 export const TransactionsContext = createContext<TransactionsContextData>(
    {} as TransactionsContextData
@@ -33,9 +33,18 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {    
-    api.post('/transactions', transaction)
-  }
+  async function createTransaction(transactionInput: TransactionInput) {    
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    })
+    const { transaction } = response.data;
+
+    setTransactions([
+      ...transactions,
+      transaction,
+    ]);
+  };
 
   return (
     <TransactionsContext.Provider value={{ transactions, createTransaction }}>
